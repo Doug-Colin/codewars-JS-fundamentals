2ffeed622b6b5229faf4fe33aac6dff62776f288
Douglas Colin Moore <dougcolin@gmail.com>
2022-06-05 20:00:08 -0400

    Codewars challenge 0020

---
+++ solution0020.js
@@ -0,0 +1,5 @@
+// ////////////// 8kyu Odd & Even  ////////////
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function oddOrEven(num) {
+    return num % 2 == 0? "Even" : "Odd"
+  }
