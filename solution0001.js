2b3efdd83710f0f15890f892b7070cf63b05b7df
Douglas Colin Moore <dougcolin@gmail.com>
2022-06-12 18:55:20 -0400

    Codewars solution0001

---
+++ solution0001.js
@@ -0,0 +1,7 @@
+alert('Working')
+
+// When doing codewars, copy and throw test cases into here
+// If you need to view the answer in Codewars, still push it to GH for the green square
+
+const points = [40, 100, 1, 5, 25, 10];
+console.log(points[0]);
