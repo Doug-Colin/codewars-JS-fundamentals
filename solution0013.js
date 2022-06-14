d9206d3c74412bd72b481c6adb7e1b96fe366a89
Douglas Colin Moore <dougcolin@gmail.com>
2022-06-13 20:05:12 -0600

    Codewars challenge 0013

--- solution0013.js
+++ solution0013.js
@@ -6,7 +6,7 @@
 // How this works:
 // In order to make a loop iterate from the back to front of an array, we need to have the starting point at the end. How do we describe that? Remember that 'i' represents the index #, and arr.length returns the amount of elements. Since the index starts at 0, in any array, the index # of the final element will be the # of elements - 1.
 //So you're iterating through, and on second iteration the array is one less long, but it keeps goign with that -1 index
-// then you use unshift to add to the beginning of the new array.   
+// then you use unshift to add to the beginning of the new array...  
 
 let origArray = [1,3,5,7,9];
 
