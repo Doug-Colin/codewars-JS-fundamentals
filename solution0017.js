210da03af409fc3ad5ebfeea45f080ec67b85e75
Douglas Colin Moore <dougcolin@gmail.com>
2022-06-23 12:47:23 -0600

    Codewars challenge 0016

---
+++ solution0017.js
@@ -0,0 +1,8 @@
+//////////////7kyu Flatten and Sort an Array //////////////
+// note- this may be an incomplete answer - check codepen for your code from this challenge and update 
+
+function flattenAndSort(array) {
+    return [].concat ((a,b) => a-b)
+  
+  }
+  
