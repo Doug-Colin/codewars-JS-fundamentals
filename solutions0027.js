285d3473dc763c0ed283fb52bd4f26a060a1f731
Douglas Colin Moore <dougcolin@gmail.com>
2022-07-02 15:03:30 -0400

    Codewars Challenge 0027

---
+++ solutions0027.js
@@ -0,0 +1,30 @@
+/*
+--------------- 8 Kyu - Convert a Number to a String ------------------
+Instructions:
+
+We need a function that can transform a number into a string.
+What ways of achieving this do you know?
+
+Examples:
+123 --> "123"
+999 --> "999"
+
+--------------
+Sample Tests
+
+const assert = require('chai').assert;
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(numberToString(67), '67');
+  });
+});
+
+--------------
+
+Psuedo Code:
+-use .toString()
+*/
+
+function numberToString(num) {
+    return num.toString();
+    }
