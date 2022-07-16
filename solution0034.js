42ce11076b4fc774c2c2dee4015d9a480b53bc19
Douglas Colin Moore <dougcolin@gmail.com>
2022-07-16 16:33:19 -0400

    Codewars Challenge 0034

---
+++ solution0034.js
@@ -0,0 +1,40 @@
+/*
+--------------- 8 Kyu - Century From Year ------------------
+
+Instructions:
+
+The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+
+-------------
+Sample Tests
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(century(1705), 18, 'Testing for year 1705');
+Test.assertEquals(century(1900), 19, 'Testing for year 1900');
+Test.assertEquals(century(1601), 17, 'Testing for year 1601');
+Test.assertEquals(century(2000), 20, 'Testing for year 2000');
+Test.assertEquals(century(89), 1, 'Testing for year 89');
+  });
+});
+
+--------------
+
+Psuedo Code:
+- Divide year by 100 to move decimal point
+- Use Math.ceil() to round that up to next integer
+- return
+*/
+
+function century(year) {
+    return Math.ceil(year/100); 
+  }
