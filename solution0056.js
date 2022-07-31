b4f472cfcd574016d2581d07b19cabc2f6b7c4b0
Doug-Colin <102709219+Doug-Colin@users.noreply.github.com>
2022-07-30 23:17:44 -0400

    'Solution0056.js'

---
+++ solution0056.js
@@ -0,0 +1,40 @@
+/*
+--------------- 8 Kyu - Invert values ------------------
+
+Instructions:
+
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == []
+You can assume that all values are integers. Do not mutate the input array/list.
+
+-------------
+Sample Tests
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Invert array values",() => {
+  it("Basic Tests", () => {
+    assert.deepEqual(invert([1,2,3,4,5]), [-1,-2,-3,-4,-5]);
+    assert.deepEqual(invert([1,-2,3,-4,5]), [-1,2,-3,4,-5]);
+    assert.deepEqual(invert([]), []);
+    assert.deepEqual(invert([0]), [-0]);
+  });
+});
+
+
+--------------
+
+Psuedo Code:
+-use .map() to multiply each array element by -1 
+-array.map(x => x * -1)
+
+*/
+
+function invert(array) {
+    return array.map(x => x * -1); 
+  }
