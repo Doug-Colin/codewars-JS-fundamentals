05b804d9bf870ef69abded829042e75993343ed3
Douglas Colin Moore <dougcolin@gmail.com>
2022-08-10 23:49:12 -0400

    Codewars Challenge 0070

---
+++ solution0071.js
@@ -0,0 +1,46 @@
+/*
+--------------- 8 Kyu - Beginner Series #2 Clock ------------------
+
+Instructions:
+
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+
+-------------
+Sample Tests
+
+describe("Fixed Tests", () => {
+  it("Tests", () => {
+    Test.assertEquals(past(0,1,1),61000)
+    Test.assertEquals(past(1,1,1),3661000)
+    Test.assertEquals(past(0,0,0),0)
+    Test.assertEquals(past(1,0,1),3601000)
+    Test.assertEquals(past(1,0,0),3600000)
+  });
+});
+
+--------------
+
+Psuedo Code:
+- find millisecs per hour, minute, second
+- use those numbers to write function which multiplies each
+------
+
+*/
+
+function past(h, m, s){
+    return ((h*3600000) + (m*60000) + (s*1000) )
+  }
