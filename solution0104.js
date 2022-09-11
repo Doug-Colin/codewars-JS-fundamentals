a6f9c97913e41ccb6d573567272d1f4270a71069
Douglas Colin Moore <dougcolin@gmail.com>
2022-09-10 23:59:57 -0400

    Codewars Challenge 0104

---
+++ solution0104.js
@@ -0,0 +1,26 @@
+/*
+---------------  Kyu -  ------------------
+
+Instructions:
+
+
+
+-------------
+Sample Tests
+
+
+
+--------------
+PREP
+
+Parameters:
+
+Return:
+
+Example:
+
+Psuedo Code:
+
+
+
+*/
